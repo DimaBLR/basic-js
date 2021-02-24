10153b614f8f551a2545a7ce03c420709168b40d
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-24 12:37:42 +0300

    refactor: short code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,23 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-    if(!options.separator) options.separator = '+';
     if(!options.additionSeparator) options.additionSeparator = '|';
-    if(options.addition === null) options.addition = 'null';
-    if(str === null) str = 'null';
-    if (options.additionRepeatTimes == undefined) {options.additionRepeatTimes = 1;}
-    if (options.repeatTimes == undefined) {options.repeatTimes = 1;}
+    if(!options.separator) options.separator = '+';    
+    if(options.addition === null) options.addition = 'null'
+    
+    return new Array(options.repeatTimes).fill(str + new Array(options.additionRepeatTimes)
+        .fill(options.addition).join(options.additionSeparator)).join(options.separator);
 
-    let arr = [], arr2 = [];
-        for (let i = 0 ;i < options.additionRepeatTimes; i++) {
-            arr.push(options.addition);
-        }
-        arr = arr.join(options.additionSeparator).split('');
-        arr.unshift(str);
-        arr = arr.join('');
-
-        for (let i = 0 ;i < options.repeatTimes; i++) {
-            arr2.push(arr);
-        }
-return arr2.join(options.separator);
 };
