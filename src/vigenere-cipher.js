1cfb44816130b2284f2a9539ed2ea92e53d7ec8c
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-24 15:43:09 +0300

    init: add code

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+function crypt(str, key, cipher, reverse) {
+  let a = "A".charCodeAt(0), z = "Z".charCodeAt(0), i = 0;
+    str = str.toUpperCase().split("").map(it => {
+      if (it.charCodeAt(0) < a || it.charCodeAt(0) > z) return it;
+      return String.fromCharCode(a + (it.charCodeAt(0) - a + (cipher ? 1 : -1) *
+          (key.toUpperCase().charCodeAt(i++ % key.length) - a) + (z - a + 1)) % (z - a + 1));
+    });
+      if (reverse) str.reverse();
+      return str.join("");
+}
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.reverse = direct == false;
+  }
+  encrypt(str, key) {
+    return crypt(str, key, true, this.reverse);
+  }
+  decrypt(str, key) {
+    return crypt(str, key, false, this.reverse);
   }
 }
 
